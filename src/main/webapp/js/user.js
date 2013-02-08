e11d61f6b01ce0063aa46a5258d30ba217393c26
Ramanathan Balakrishnan <sreeramanathan@gmail.com>
2013-02-08 16:01:14 +0000

    More javascript

--- src/main/webapp/js/user.js
+++ src/main/webapp/js/user.js
@@ -1,6 +1,12 @@
 var User = function() {
     var self = {};
 
+    var emptyTheUser = function(user) {
+        user.addClass("empty");
+        user.find(".forename").val("Forename");
+        user.find(".surname").val("Surname");
+    }
+
     var textboxFocus = function() {
         var user = $(this).parent(".user");
         if (user.hasClass("empty")) {
@@ -12,9 +18,7 @@ var User = function() {
     var textboxBlur = function() {
         var user = $(this).parent(".user");
         if (user.find(".forename").val() === "" && user.find(".surname").val() === "") {
-            user.addClass("empty");
-            user.find(".forename").val("Forename");
-            user.find(".surname").val("Surname");
+            emptyTheUser(user);
         }
     };
 
@@ -29,12 +33,42 @@ var User = function() {
         $(".forename,.surname").unbind();
         $(".forename,.surname").focus(textboxFocus);
         $(".forename,.surname").blur(textboxBlur);
-    }
+    };
+
+    var createSuccess = function(user) {
+        user.addClass("submitted");
+    };
+
+    var deleteSuccess = function(user) {
+        user.removeClass("submitted");
+        emptyTheUser(user);
+    };
+
+    var ajaxForUser = function(type, successHandler, user) {
+        $.ajax({
+            url: "user",
+            type: type,
+            success: function() {
+                successHandler(user);
+            }
+        });
+    };
+
+    var action = function() {
+        var element = $(this);
+        var parent = element.parent();
+        if (parent.hasClass("submitted")) {
+            ajaxForUser("DELETE", deleteSuccess, parent);
+        } else if (!element.hasClass("empty")) {
+            ajaxForUser("POST", createSuccess, parent);
+        }
+    };
 
     self.load = function() {
         $(".forename,.surname").focus(textboxFocus);
         $(".forename,.surname").blur(textboxBlur);
         $(".add-user").click(addUser);
+        $(".action").click(action);
     };
 
     return self;
