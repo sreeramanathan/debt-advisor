8c74ab2b369f62570a7e25f8051dafedacdf404b
Ramanathan Balakrishnan <sreeramanathan@gmail.com>
2013-02-08 12:48:38 +0000

    escalate hard-coded data from view to controller

--- src/main/java/debt_advisor/controllers/UserController.java
+++ src/main/java/debt_advisor/controllers/UserController.java
@@ -1,12 +1,25 @@
 package debt_advisor.controllers;
 
+import debt_advisor.models.User;
+import org.stringtemplate.v4.ST;
+
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import static debt_advisor.utils.StringTemplate.template;
 
 public class UserController {
     public void index(HttpServletResponse resp) throws IOException {
-        resp.getWriter().write(template("user").render());
+        ST template = template("user");
+        List<User> users = new ArrayList<User>();
+        users.add(new User("Rajgopal", "Menon"));
+        users.add(new User("Ramanathan", "Balakrishnan"));
+        users.add(new User());
+        users.add(new User());
+        users.add(new User());
+        template.add("users", users);
+        resp.getWriter().write(template.render());
     }
 }
---
+++ src/main/java/debt_advisor/models/User.java
@@ -0,0 +1,31 @@
+package debt_advisor.models;
+
+public class User {
+    private String forename;
+    private String surname;
+    private boolean empty;
+
+    public User(String forename, String surname) {
+        this.forename = forename;
+        this.surname = surname;
+        this.empty = false;
+    }
+
+    public User() {
+        this.forename = "forename";
+        this.surname = "surname";
+        this.empty = true;
+    }
+
+    public String getForename() {
+        return forename;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public boolean isEmpty() {
+        return empty;
+    }
+}
